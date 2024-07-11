4aee6b29075fa656d2e597daf2b957a4ca5612e9
Glenn Francis <xglennfrancisx@gmail.com>
2024-07-11 11:29:48 +1000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import CoinTable from '@/components/coin-table';
 import { HOME_ENDPOINT } from '@/const';
-import { type Coin } from '@/types';
 
-async function getData(): Promise<Coin[]> {
+async function getData() {
   const res = await fetch(HOME_ENDPOINT);
   if (!res.ok) return [];
   return res.json();
 }
 
-async function HomePage() {
+export default async function HomePage() {
   const coins = await getData();
   return <CoinTable coins={coins} />;
 }
-
-export default HomePage;
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,20 +4,18 @@ function Header() {
   return (
     <header className='min-h-[250px] w-full bg-gradient-to-r from-purple-800 via-violet-900 to-purple-800 p-8 sm:min-h-[350px]'>
       <div className='container max-w-2xl'>
-        <div className='space-y-3 pt-10 pb-10 text-center sm:pt-20'>
+        <div className='pt-10 pb-10 space-y-3 text-center sm:pt-20'>
           <Link href='/'>
-            <h1 className='text-4xl font-extrabold hover:opacity-90'>
-              CoinWatch
-            </h1>
+            <h1 className='text-5xl font-bold hover:opacity-90'>CoinWatch</h1>
           </Link>
           <div>
             <a
               href='https://github.com/gmf001/coin-watch'
               target='_blank'
-              className='text-xs font-bold uppercase text-purple-100 hover:text-purple-200'
+              className='text-base font-bold text-purple-400 uppercase hover:text-purple-200'
               rel='noreferrer'
             >
-              View on Github
+              View Github Repo
             </a>
           </div>
         </div>
